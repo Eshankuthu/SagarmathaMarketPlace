a3d808b645252a849b303078296f1f86990ba087
eshankuthu <kuthu.eshan@gmail.com>
2018-04-25 20:29:12 -0500

    Addedd Pojo class

--- src/main/java/com/sagarmatha/domain/Address.java
+++ src/main/java/com/sagarmatha/domain/Address.java
@@ -1,6 +1,5 @@
 package com.sagarmatha.domain;
 
-import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,23 +9,16 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "ADDRESS")
-public class Address implements Serializable{
+public class Address{
 	
-	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private long addressId;
+	private int addressId;
 	
 	private String street;
 
-	public long getAddressId() {
-		return addressId;
-	}
-
-	public void setAddressId(long addressId) {
-		this.addressId = addressId;
-	}
+	
 
 	public String getStreet() {
 		return street;
