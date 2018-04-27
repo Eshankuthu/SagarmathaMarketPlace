45db2207a47e7a12bc88e1ff4db85a5db1b20ca8
ciiay <redrainyday@gmail.com>
2018-04-26 21:46:03 -0500

    JSP and resource files

--- src/main/java/com/sagarmatha/ProductController.java
+++ src/main/java/com/sagarmatha/ProductController.java
@@ -9,5 +9,25 @@ public class ProductController {
 	public String getIndex() {
 		return "index";
 	}
+	
+	@RequestMapping("/products")
+	public String showProductsPage() {
+		return "products";
+	}
+
+	@RequestMapping("/shoppingcart")
+	public String showShoppingCartPage() {
+		return "shoppingcart";
+	}
+	
+	@RequestMapping("/productdetail")
+	public String showProductDetailPage() {
+		return "productdetail";
+	}
+	
+	@RequestMapping("/checkout")
+	public String showCheckoutPage() {
+		return "checkout";
+	}
 
 }
